4d93ad2a3580713a73ad1e2f65e23421d854cfda
iago-mattos <iago.mattos@prognum.com.br>
2026-06-09 18:08:08 -0300

    first commit

---
+++ cypress/e2e/setup.cy.ts
@@ -0,0 +1,7 @@
+describe("Configuracao do Cypress", () => {
+  it("executa testes escritos em TypeScript", () => {
+    const framework: string = "Cypress";
+
+    expect(framework).to.equal("Cypress");
+  });
+});
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,1 @@
+// Comandos e configuracoes globais dos testes E2E devem ser importados aqui.
